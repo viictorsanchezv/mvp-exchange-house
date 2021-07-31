1917187cdea5fbaeb8ee59a6769597a76852aac6
viictorsanchezv <ingsanchez423@gmail.com>
2021-07-31 18:13:40 -0400

    cambios en local

--- resources/views/livewire/create-user.blade.php
+++ resources/views/livewire/create-user.blade.php
@@ -11,15 +11,15 @@
                         <table class="min-w-full divide-y divide-gray-200">
                             <thead class="bg-gray-50">
                                 <tr>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Nombre
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Estatus
-                                </th>
-                                <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-                                    Rol
-                                </th>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Nombre
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Estatus
+                                    </th>
+                                    <th scope="col" class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                        Rol
+                                    </th>
                                 </tr>
                             </thead>
                             <tbody class="bg-white divide-y divide-gray-200">
@@ -55,7 +55,7 @@
                                 <!-- More people... -->
                             </tbody>
                         </table>
-                        {{ $users->links() }}
+                        <!-- {{ $users->links() }} -->
                     </div>
                 </div>
             </div>
