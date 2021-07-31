2f2d2dc18570ec9bbe402173605ab62b1f02ac8c
viictorsanchezv <ingsanchez423@gmail.com>
2021-07-31 16:45:29 -0400

    hash make password fix in user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -7,6 +7,8 @@ use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
 use Laravel\Jetstream\Features;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserFactory extends Factory
 {
@@ -29,7 +31,7 @@ class UserFactory extends Factory
             'user' => $this->faker->unique()->userName(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('micontraseña'), // password
             'remember_token' => Str::random(10),
             'rol_id' => rand(1,2),
         ];
