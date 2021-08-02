f1ed5fd96b0a95a314f9af4b5b6254a6d03f5aec
viictorsanchezv <ingsanchez423@gmail.com>
2021-08-02 10:00:12 -0400

    changed service provider file

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         if (config('app.env') === 'production') {
-            URL::forceSchema('https');
+            \URL::forceScheme('https');
         }
     }
 }
