aba0587b2510829807aca46097a327f35b96538f
viictorsanchezv <ingsanchez423@gmail.com>
2021-08-10 17:50:22 -0400

    added dinamic data in product layout

--- resources/views/livewire/detailed-transaction.blade.php
+++ resources/views/livewire/detailed-transaction.blade.php
@@ -9,7 +9,7 @@
                             src="{{ asset('images/tailwind-css-logo.svg') }}"
                             alt="">
                     </div>
-                    <h1 class="text-gray-900 font-bold text-xl leading-8 my-1">Jane Doe</h1>
+                    <h1 class="text-gray-900 font-bold text-xl leading-8 my-1">{{ $transaction->user->name; }}</h1>
                     <h3 class="text-gray-600 font-lg text-semibold leading-6">Detalles de la transacción</h3>
                     <p class="text-sm text-gray-500 hover:text-gray-600 leading-6">Aquí puedes observar los detalles
                         de tu transaccion y editarla de ser necesario.
@@ -17,7 +17,7 @@
                     <ul
                         class="bg-gray-100 text-gray-600 hover:text-gray-700 hover:shadow py-2 px-3 mt-3 divide-y rounded shadow-sm">
                         <li class="flex items-center py-3">
-                            <span>Estado</span>
+                            <span>{{ $transaction->statu->name }} </span>
                             <span class="ml-auto"><span
                                     class="bg-green-500 py-1 px-2 rounded text-white text-sm">Active</span></span>
                         </li>
@@ -44,19 +44,19 @@
                             <div class="grid md:grid-cols-2 text-sm">
                                 <div class="grid grid-cols-2">
                                     <div class="px-4 py-2 font-semibold">Cliente Envía</div>
-                                    <div class="px-4 py-2">{{ $transaction->id }}</div>
+                                    <div class="px-4 py-2">{{ $transaction->client->name; }}</div>
                                 </div>
                                 <div class="grid grid-cols-2">
                                     <div class="px-4 py-2 font-semibold">Cliente Recibe</div>
-                                    <div class="px-4 py-2">Nombre aqui</div>
+                                    <div class="px-4 py-2">{{ $transaction->client_receive->name; }}</div>
                                 </div>
                                 <div class="grid grid-cols-2">
                                     <div class="px-4 py-2 font-semibold">Monto</div>
-                                    <div class="px-4 py-2">Numerito</div>
+                                    <div class="px-4 py-2"> {{ $transaction->money_sent; }}</div>
                                 </div>
                                 <div class="grid grid-cols-2">
                                     <div class="px-4 py-2 font-semibold">Fecha a entregar el dinero</div>
-                                    <div class="px-4 py-2">Cuando haya efectivo</div>
+                                    <div class="px-4 py-2"> {{ $transaction->date_end; }}</div>
                                 </div>
                             </div>
                         </div>
