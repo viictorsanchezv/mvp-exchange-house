d448a1ce312c6749c25c856a6b90eb72fcf96744
elianabcrr <eliana.becerra0916@gmail.com>
2021-08-11 00:24:23 -0400

    system notification

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Models;
+use App\Models\Client;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -8,4 +9,10 @@ use Illuminate\Database\Eloquent\Model;
 class Country extends Model
 {
     use HasFactory;
+
+
+    public function clients()
+    {
+        return $this->hasMany(Client::class);
+    }
 }
